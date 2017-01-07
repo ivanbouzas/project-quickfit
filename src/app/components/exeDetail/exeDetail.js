b6665e58e6fd422c4f589c1ff2fe9971094a34b7
ivanbouzas <ivan.bouzas@outlook.com>
2017-01-07 01:14:33 +0100

    Popup for detail of an exercise functional

--- src/app/components/exeDetail/exeDetail.js
+++ src/app/components/exeDetail/exeDetail.js
@@ -1,11 +1,30 @@
-function exeDetailController() {
+function exeDetailController($http) {
   $ctrl = this;
   $ctrl.close = function () {
     angular.element('#popDetailExe').attr('style', 'display:none;');
   };
-  $http.get('https://wger.de/api/v2/exercise/?language=2&format=json').then(function (response) {
-    $ctrl.exercise = angular.fromJson(response.data.results);
-  });
+  $ctrl.$onChanges = function (argument) {
+    if (angular.isDefined($ctrl.exeDetailId)) {
+      $http.get('https://wger.de/api/v2/exercise/' + $ctrl.exeDetailId + '/?language=2&format=json').then(function (response) {
+        $ctrl.exercise = angular.fromJson(response.data);
+        $http.get('https://wger.de/api/v2/exercisecategory/' + $ctrl.exercise.category + '/?language=2&format=json').then(function (response) {
+          $ctrl.category = angular.fromJson(response.data);
+        });
+        angular.element('#descExe').html('Description : ' + $ctrl.exercise.description);
+      });  
+      $http.get('https://wger.de/api/v2/exerciseimage/?exercise='+ $ctrl.exeDetailId +'&format=json').then(function (response) {        
+          if (response.data.count == 0) {
+            $ctrl.noImages = true;
+            $ctrl.image1 = null;
+            $ctrl.image2 = null;
+          } else {
+            var images = angular.fromJson(response.data.results);
+            $ctrl.image1 = images[0].image;
+            $ctrl.image2 = images[1].image;
+          } 
+      });
+    }
+  }
 }
 
 angular
