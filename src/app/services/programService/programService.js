28e1d56514dd0a91bdfb74c45276bc8c8ea220f6
AlexandruOprean <alexandr.oprean@he-arc.ch>
2017-01-14 17:34:40 +0100

    Updates CRLF -> LF

--- src/app/services/programService/programService.js
+++ src/app/services/programService/programService.js
@@ -7,10 +7,10 @@ function ProgramService() {
     if (localStorage.getItem(pKey) !== null) {
       programs = angular.fromJson(localStorage.getItem(pKey));
       angular.forEach(programs, function (value, key, obj) {
-        value.exeUnitTime = new Date(value.exeUnitTime != null ? value.exeUnitTime : '0');
-        value.exeUnitRest = new Date(value.exeUnitRest != null ? value.exeUnitRest : '0');
-        value.exeObjTime = new Date(value.exeObjTime != null ? value.exeObjTime : '0');
-        value.exeObjTimeInc = new Date(value.exeObjTimeInc != null ? value.exeObjTimeInc : '0');
+        value.exeUnitTime = new Date(value.exeUnitTime !== null ? value.exeUnitTime : '0');
+        value.exeUnitRest = new Date(value.exeUnitRest !== null ? value.exeUnitRest : '0');
+        value.exeObjTime = new Date(value.exeObjTime !== null ? value.exeObjTime : '0');
+        value.exeObjTimeInc = new Date(value.exeObjTimeInc !== null ? value.exeObjTimeInc : '0');
       });
     }
     return programs;
