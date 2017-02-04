24b51487333a5e1a046b6a85eb322594740a2d50
ivanbouzas <ivan.bouzas@outlook.com>
2017-02-04 19:04:13 +0100

    Fixed timer when double start clic

--- src/app/components/timer/timer.js
+++ src/app/components/timer/timer.js
@@ -12,38 +12,40 @@ function timerController($interval) {
     }
   };
   $ctrl.start = function () {
-    angular.element('#timer').removeClass('flash');
-    angular.element('#timer').addClass('pulse');
-    $ctrl.timer = $interval(function () {
-      if ($ctrl.now !== 0 && Math.floor(($ctrl.now + 1000) / 1000) !== Math.floor(Date.now() / 1000)) {
-        $ctrl.diff = Math.floor((Date.now() - $ctrl.now) / 1000);
-      } else {
-        $ctrl.diff = 1;
-      }
-      $ctrl.now = Date.now();
-      $ctrl.sec0 += $ctrl.diff;
-      if ($ctrl.sec0 >= 10) {
-        $ctrl.sec00 += Math.floor($ctrl.sec0 / 10);
-        $ctrl.sec0 -= Math.floor($ctrl.sec0 / 10) * 10;
-        if ($ctrl.sec00 >= 6) {
-          $ctrl.min0 += Math.floor($ctrl.sec00 / 6);
-          $ctrl.sec00 -= Math.floor($ctrl.sec00 / 6) * 6;
-          if ($ctrl.min0 >= 10) {
-            $ctrl.min00 += Math.floor($ctrl.min0 / 10);
-            $ctrl.min0 -= Math.floor($ctrl.min0 / 10) * 10;
+    if (angular.isUndefined($ctrl.timer)) {
+      angular.element('#timer').removeClass('flash');
+      angular.element('#timer').addClass('pulse');
+      $ctrl.timer = $interval(function () {
+        if ($ctrl.now !== 0 && Math.floor(($ctrl.now + 1000) / 1000) !== Math.floor(Date.now() / 1000)) {
+          $ctrl.diff = Math.floor((Date.now() - $ctrl.now) / 1000);
+        } else {
+          $ctrl.diff = 1;
+        }
+        $ctrl.now = Date.now();
+        $ctrl.sec0 += $ctrl.diff;
+        if ($ctrl.sec0 >= 10) {
+          $ctrl.sec00 += Math.floor($ctrl.sec0 / 10);
+          $ctrl.sec0 -= Math.floor($ctrl.sec0 / 10) * 10;
+          if ($ctrl.sec00 >= 6) {
+            $ctrl.min0 += Math.floor($ctrl.sec00 / 6);
+            $ctrl.sec00 -= Math.floor($ctrl.sec00 / 6) * 6;
+            if ($ctrl.min0 >= 10) {
+              $ctrl.min00 += Math.floor($ctrl.min0 / 10);
+              $ctrl.min0 -= Math.floor($ctrl.min0 / 10) * 10;
+            }
           }
         }
-      }
-      if ($ctrl.restPeriod !== 0) {
-        $ctrl.totaltimesec = $ctrl.sec0 + $ctrl.sec00 * 10 + $ctrl.min0 * 60 + $ctrl.min00 * 600;
-        if ($ctrl.totaltimesec >= $ctrl.restPeriod) {
-          $ctrl.reset();
-          $ctrl.stop();
-          angular.element('#timer').css('color', 'green');
-          angular.element('#timer').css('font-weight', 'bold');
+        if ($ctrl.restPeriod !== 0) {
+          $ctrl.totaltimesec = $ctrl.sec0 + $ctrl.sec00 * 10 + $ctrl.min0 * 60 + $ctrl.min00 * 600;
+          if ($ctrl.totaltimesec >= $ctrl.restPeriod) {
+            $ctrl.reset();
+            $ctrl.stop();
+            angular.element('#timer').css('color', 'green');
+            angular.element('#timer').css('font-weight', 'bold');
+          }
         }
-      }
-    }, 1000);
+      }, 1000);
+    }
   };
   $ctrl.stop = function () {
     angular.element('#timer').removeClass('pulse');
